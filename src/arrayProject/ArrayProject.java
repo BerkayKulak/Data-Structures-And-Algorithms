9dc4c6736a78f3a587b6f5afb13d40ff5960eb3b
DESKTOP-6VRP7TA\MSI <kulakberkay15@gmail.com>
2023-01-28 11:03:52 +0300

    deleted Console

--- src/arrayProject/ArrayProject.java
+++ src/arrayProject/ArrayProject.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class ArrayProject {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
         System.out.print("How many days' temperatures? ");
         int numDays = console.nextInt();
         int[] temps = new int[numDays];
